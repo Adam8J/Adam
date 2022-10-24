b3dceacad3701244f0459eb6db2edcbf031a2fd9
Adam8J <adam.jurasz@wp.pl>
2022-10-24 21:03:33 +0200

    podaj wartość

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Adam";
-        System.out.println("Hello "+name);
-
+        Scanner sc =new Scanner(System.in);
+        System.out.print("podaj pierwszą wartość: ");
+        int number1 = sc.nextInt();
+        System.out.print("Podaj drugą wartość: ");
+        int number2 = sc.nextInt();
+        System.out.println("Wynik to:" +  (number1 +number2));
     }
 }
